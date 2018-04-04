2fdd131f4ab62e331f39293bd79672a7f56baa98
Pavel.Gavrikov <pavel.g@powercode.us>
2018-04-04 15:51:49 +0300

    Added logs

--- install-docker.sh
+++ install-docker.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run with sudo"
+    exit 1
+fi 
 
 #Install docker & docker-compose
-sudo apt-get install && sudo apt-get install docker docker-compose
+apt-get install && sudo apt-get install docker docker-compose
 #Add user to group docker:
-sudo usermod -a -G docker $SUDO_USER
+usermod -a -G docker $SUDO_USER
 
 #Create network:
-sudo docker network create front
+docker network create front
   
   
 #Add to /etc/hosts 
-sudo cp /etc/hosts /etc/hosts.bak
+cp /etc/hosts /etc/hosts.bak
 DOCKER_IP=`ifconfig docker0 | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
-sudo echo "$DOCKER_IP  backend.net" >> /etc/hosts
-sudo echo "$DOCKER_IP  db.backend.net" >> /etc/hosts
+echo "$DOCKER_IP  backend.net" >> /etc/hosts
+echo "$DOCKER_IP  db.backend.net" >> /etc/hosts
 
-  
+ 
 #Starting container: 
 echo "Starting Docker containers..."
-docker-compose -p backend up -d --build
+#docker-compose -p backend up -d --build
 
+chown $SUDO_USER: -R ./
 echo "To stop containers, enter command: docker-compose down"
-echo "Start the containers with the command:docker-compose up -d"
+echo "Start the containers with the command: docker-compose up -d"
